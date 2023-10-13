a129cd594755365c15768013c3c0f01dd3583fe8
ysjang <changys1212@naver.com>
2023-10-13 16:45:59 +0900

    add config-server for kubeBoard

---
+++ service-service/src/test/kotlin/miniproject/kubeBoard/serviceservice/ServiceServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package miniproject.kubeBoard.serviceservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
