aaa83874aa892752bd344c4ae17f2560fd7a1dcc
ysjang <changys1212@naver.com>
2023-11-28 18:29:20 +0900

    [front] 서비스 생성 모달 페이지 추가

--- frontend/src/components/common/CreateModal.vue
+++ frontend/src/components/common/CreateModal.vue
@@ -15,10 +15,43 @@
                     <p style="font-size:14px">{{resource}} 명</p>
                     <input id="name" v-model="name">
                 </div>
-                <div class="col-4">
+                <div class="col-4" v-if="resource==='deployment'">
                     <p style="font-size:14px">replica 수</p>
                     <input type="number" id="replica"
                            v-model="replica" min="1" >
+                </div>
+                <div class="col-4" v-if="resource==='service'" >
+                    <p style="font-size:14px" >Label</p>
+                    <select v-model="label" class="custom-select">
+                        <option v-for="(label,index) in labellist" :key="index" :value="label">
+                        {{label}}
+                        </option>
+                    </select>
+                </div>
+            </div>
+            <div class="row mt-4" v-if="resource==='service'">
+                <div class="col-3">
+                    <p style="font-size:14px">type</p>
+                    <select v-model="type" class="service-select" >
+                        <option value="LoadBalancer" selected>LoadBalancer</option>
+                        <option value="ClusterIP">ClusterIP</option>
+                    </select>
+                </div>
+                <div class="col-3">
+                    <p style="font-size:14px">protocol</p>
+                    <select v-model="protocol" class="service-select">
+                        <option value="TCP" selected>TCP</option>
+                        <option value="UDP">UDP</option>
+                    </select>
+                </div>
+                <div class="col-3">
+                    <p style="font-size:14px">port</p>
+                    <input type="number" id="port" v-model="port" min="1" class="service-select">
+                </div>
+                <div class="col-3">
+                    <p style="font-size:14px">target port</p>
+                    <input type="number" id="targetport"
+                           v-model="targetport" min="1" class="service-select" >
                 </div>
             </div>
             <hr/>
@@ -30,24 +63,78 @@
     </div>
 </template>
 <script>
-    import {ref} from "vue";
+import {onMounted, ref} from "vue";
+import axios from "axios";
     export default {
         props:['resource'],
         emits:['createResource'],
         setup(props,{emit}){
             const name = ref("");
             const namespace = ref("");
+            const protocol = ref("");
             const replica = ref(1);
+            const port = ref(80);
+            const targetport = ref(80);
+            const labellist = ref({});
+            const label = ref("");
+            const type = ref("");
             const createResource = ()=>{
-                emit('createResource', {name:name.value,
+
+                if (!name.value || !namespace.value) {
+                    alert("이름과 namespace 를 확인주세요.");
+                    return;
+                }
+                if(props.resource === 'deployment'){
+                    if(!replica.value) {
+                        alert("레플리카 수를 확인해주세요")
+                        return;
+                    }
+                }
+                if(props.resource === 'service'){
+                    if(!port.value || !targetport.value){
+                        alert("포트를 확인해주세요")
+                        return;
+                    }
+                    if(!type.value ){
+                        alert("타입을 지정해주세요")
+                        return;
+                    }
+                    if(!protocol.value ){
+                        alert("프로토콜을 지정해주세요")
+                        return;
+                    }
+                    if(!label.value){
+                        alert("서비스를 적용 할 레이블을 확인해주세요")
+                        return;
+                    }
+                }
+                emit('createResource', {
+                    name:name.value,
                     namespace:namespace.value,
-                replica:replica.value});
+                    replica:replica.value,
+                    port:port.value,
+                    type:type.value,
+                    targetport:targetport.value,
+                    protocol:protocol.value,
+                    label:label.value});
             };
+            const getLabelList = async() => {
+                try {
+                    const {data} = await  axios.get(
+                        `/pod-service/getLabels`);
+                    labellist.value = data
+                }catch (err){
+                    console.log(err);
+                }
+            }
+            onMounted(()=> {
+                getLabelList();
+            })
             const onclose = () =>{
                 emit('close');
             }
             return{
-                createResource,onclose,name, namespace,replica
+                createResource,onclose,name, namespace,replica,protocol,port,targetport,labellist,label,type
             };
         }
     }
@@ -74,10 +161,8 @@
         margin: 0 auto;
         padding: 20px;
     }
-    .warningtext{
-        background-color: #6E6E6E;
-        color:white;
-        font-size: 13px;
-        padding: 5px;
+    .service-select{
+        width: 120px;
+        height: 30px;
     }
 </style>
--- frontend/src/components/service/Service.vue
+++ frontend/src/components/service/Service.vue
@@ -1,5 +1,12 @@
 <template>
+    <div>
   <table-slot header="서비스">
+      <template v-slot:create>
+          <button class="btn btn-outline-secondary btn-outline-bold" @click="openCreateModal">
+              <span v-if="createLoading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+              <span v-if="!createLoading">생성하기</span>
+          </button>
+      </template>
     <base-spinner v-if="isLoading"></base-spinner>
     <table v-else>
       <thead>
@@ -10,6 +17,7 @@
           <th>생성시간</th>
           <th>레이블</th>
           <th>상세</th>
+          <th>삭제</th>
       </tr>
       </thead>
         <tbody>
@@ -25,6 +33,12 @@
             <td>
                 <button type="button" class="btn btn-secondary btn-sm" @click="serviceDetail(item.serviceName)">조회</button>
             </td>
+            <td>
+                <button class="btn btn-outline-secondary btn-sm" @click="deleteService(item.serviceName, item.namespace)">
+                    <span v-if="deleteLoading[item.serviceName]" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                    <span v-if="!deleteLoading[item.serviceName]">삭제</span>
+                </button>
+            </td>
         </tr>
       </tbody>
     </table>
@@ -34,20 +48,28 @@
                       @getList="getServiceList"/>
       </template>
   </table-slot>
+    <teleport to="#modal">
+        <CreateModal v-if="showcreatemodal"
+                     :resource="resource"
+                     @createResource="createService"
+                     @close="closeModal"/>
+    </teleport>
+    </div>
 </template>
-
 <script>
-import {ref, onMounted, computed, provide} from "vue";
+import {ref, onMounted, computed, provide, reactive} from "vue";
 import LabelList from "@/components/common/LabelList.vue";
 import axios from "axios";
 import Pagination from "@/components/common/Pagination.vue";
 import router from "@/router";
 import {useRoute} from "vue-router";
+import CreateModal from "@/components/common/CreateModal.vue";
 
 
 export default {
     name: "serviceView",
     components: {
+        CreateModal,
         Pagination,
         LabelList,
     },
@@ -58,6 +80,10 @@ export default {
         const isLoading = ref(false);
         const limit = 5;
         const route = useRoute();
+        const showcreatemodal = ref(false);
+        const resource = "service";
+        const createLoading = ref(false);
+        const deleteLoading = reactive({});
         const setLoading = () => {
             isLoading.value = true;
         }
@@ -93,7 +119,68 @@ export default {
         const serviceDetail = (name) => {
             router.push('/service/' + name);
         };
+        //생성 모달열기
+        const openCreateModal = () => {
+            showcreatemodal.value = true;
 
+        }
+        //모달닫기
+        const closeModal = () => {
+            showcreatemodal.value = false;
+        }
+        /**
+         * service 생성하기
+         */
+        const createService = async (payload) => {
+            try {
+                createLoading.value = true;
+                closeModal();
+                console.log(payload)
+                const {data} = await axios.post('/service-service/create', {
+                    name: payload.name,
+                    namespace: payload.namespace,
+                    port: payload.port,
+                    targetport: payload.targetport,
+                    label: payload.label,
+                    protocol: payload.protocol,
+                    type: payload.type
+                });
+                if (data) {
+                    createLoading.value = false;
+                    window.location.reload();
+                }else{
+                    alert("생성실패")
+                    createLoading.value = false;
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        /**
+         * service 삭제하기
+         */
+        const deleteService = async (name, namespace) => {
+            if(confirm("정말 삭제하시겠습니까?")){
+                try {
+                    deleteLoading[name] = true
+                    const {data} = await axios.post('/service-service/delete', {
+                        name: name,
+                        namespace: namespace
+                    });
+                    if (data) {
+                        deleteLoading[name] = false
+                        alert("삭제가 완료되었습니다.")
+                        window.location.reload();
+                    }else{
+                        deleteLoading[name] = false
+                        alert("삭제 실패")
+                    }
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+
+        };
         return {
             items,
             isLoading,
@@ -101,7 +188,15 @@ export default {
             getServiceList,
             numberOfPages,
             currentPage,
-            setLoading
+            setLoading,
+            closeModal,
+            openCreateModal,
+            createService,
+            resource,
+            deleteService,
+            showcreatemodal,
+            createLoading,
+            deleteLoading
         };
     },
 };
