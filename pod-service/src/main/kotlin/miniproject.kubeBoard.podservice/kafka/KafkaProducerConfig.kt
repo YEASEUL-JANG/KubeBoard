16c4db43bff682adc4d93f66351cd6e0854a4730
YEASEUL JANG <changys1212@naver.com>
2024-02-13 19:08:07 +0900

    [pod-service] 도커파일 추가

--- pod-service/src/main/kotlin/miniproject.kubeBoard.podservice/kafka/KafkaProducerConfig.kt
+++ pod-service/src/main/kotlin/miniproject.kubeBoard.podservice/kafka/KafkaProducerConfig.kt
@@ -1,4 +1,4 @@
-package miniproject.kubeBoard.deploymentservice.kafka
+package miniproject.kubeBoard.podservice.kafka
 
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
-import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
 @Configuration
 class KafkaProducerConfig (
